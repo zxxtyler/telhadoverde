6bd9fdca3745b96d083c0867fd8a375ee962ecf2
CarlosLinux <cadudailha68@gmail.com>
2025-11-11 22:23:13 -0300

    Projeto spring implementado!

---
+++ src/main/java/com/greenroofmanager/greenroof/GreenroofApplication.java
@@ -0,0 +1,13 @@
+package com.greenroofmanager.greenroof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenroofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenroofApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenroofmanager/greenroof/GreenroofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenroofmanager.greenroof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenroofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
